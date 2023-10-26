412717f613674ea4ddcb45d311eb7454e5ea9de5
Vladislav <1@zithen.com>
2023-10-26 18:38:34 +0300

    Add generate_diff call to gendiff script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff.main_logic.generate_diff import generate_diff
 
 def main():
     args = arg_parser()
-    print(args)
+    generate_diff(args.first_file, args.second_file)
 
 
 if __name__ == '__main__':
