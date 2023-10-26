e1783bcec0da924089804f962550d36e51c79408
Vladislav <1@zithen.com>
2023-10-26 18:56:18 +0300

    Add keys from file1 to list

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -4,6 +4,11 @@ import json
 def generate_diff(file_path1, file_path2):
     file1_content = json.load(open(file_path1))
     file2_content = json.load(open(file_path2))
-    diff = {}
+    keys_ = []
 
-    print(file1_content)
+    for key in file1_content:
+        keys_.append(key)
+
+    keys_.sort()
+
+    print(keys_)
