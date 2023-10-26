5db6fb50b8ebcea49edf8d77345af4efcb71cd9a
Vladislav <1@zithen.com>
2023-10-26 18:47:36 +0300

    Add file2.json

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -2,4 +2,9 @@ import json
 
 
 def generate_diff(file_path1, file_path2):
-    print(json.load(open(file_path1)))
+    file1_content = json.load(open(file_path1))
+    file2_content = json.load(open(file_path2))
+    diff = {}
+
+    file1_content.sort()
+    print(file1_content)
