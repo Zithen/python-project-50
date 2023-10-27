38c61e9fdb28d91c8b892f447e9142e2784c6982
Vladislav <1@zithen.com>
2023-10-27 23:46:23 +0300

    Remove artefacts

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -9,25 +9,6 @@ def generate_diff(file_path1, file_path2):
     file1_keys, file2_keys = [], []
     diff_container = '{\n'
 
-
-    for key in file1_content:
-        file1_keys.append(key)
-    for key in file2_content:
-        file2_keys.append(key)
-
-    file1_keys.sort()
-
-    for key in file1_keys:
-        if key not in file2_keys:
-            diff_key = '  - ' + key + ': ' + str(file1_content[key]) + ',' + '\n'
-            diff_container += diff_key           
-        
-        elif file1_content[key] == file2_content[key]:
-            pass
-        
-        elif file1_content[key] != file2_content[key]:
-            pass
-
     diff1 = diff(file1_content, file2_content)
     diff2 = diff(file2_content, file1_content)
     print(patch(diff1, file1_content))
