f00eb5ff5d1034a74a29a8fbfa86973c9ead40a6
Vladislav <1@zithen.com>
2023-10-27 13:23:35 +0300

    Add diff logic

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -4,11 +4,27 @@ import json
 def generate_diff(file_path1, file_path2):
     file1_content = json.load(open(file_path1))
     file2_content = json.load(open(file_path2))
-    keys_ = []
+    file1_keys, file2_keys = [], []
+    diff_container = {}
+
 
     for key in file1_content:
-        keys_.append(key)
+        file1_keys.append(key)
+    for key in file2_content:
+        file2_keys.append(key)
 
-    keys_.sort()
+    file1_keys.sort()
 
-    print(keys_)
+    for key in file1_keys:
+        if key not in file2_keys:
+            key_new = '- ' + key
+            diff_container[key_new] = file1_content[key]
+        
+        elif file1_content[key] == file2_content[key]:
+            diff_container[key] = file1_content[key]
+        
+        elif file1_content[key] != file2_content[key]:
+            key_new = '+ ' + key
+            diff_container[key_new] = file2_content[key]
+
+    print(json.dumps(diff_container, indent = 4))
