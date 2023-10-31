eb0dc75044321e08b60bb0f870e485fedc2b87c2
Vladislav <1@zithen.com>
2023-10-31 19:24:53 +0300

    Back to dictdiffer

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -1,5 +1,5 @@
 import json
-from deepdiff import DeepDiff
+from deepdiff import DeepDiff, Delta
 from dictdiffer import diff, patch
 
 
@@ -9,9 +9,10 @@ def generate_diff(file_path1, file_path2):
         file2_content = json.load(file2)
     
     file1_keys, file2_keys = [], []
-    diff_container = '{\n'
+    diff_container = {}
 
     diff1 = patch(diff(file1_content, file2_content), file1_content)
     diff2 = patch(diff(file2_content, file1_content), file2_content)
-    print(diff1)
+
     print(diff2)
+    print(diff1)
