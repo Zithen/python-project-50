f4ccd3535b5ecb305eb587c79c97e9d7912af762
Vladislav <1@zithen.com>
2023-11-02 16:35:04 +0300

    Add generate_diff to init.py

--- gendiff/main_logic/generate_diff.py
+++ gendiff/main_logic/generate_diff.py
@@ -6,14 +6,14 @@ def generate_diff(file_path1, file_path2):
         file1_content = json.load(file1)
         file2_content = json.load(file2)
     diff_container = {}
-    merged_files_content = dict(sorted(({**file1_content, **file2_content}).items()))
+    merged_files_content = dict(sorted(({**file1_content, **file2_content}).items())) # noqa E501
 
     for key in merged_files_content:
         if key not in file2_content:
             diff_container['- ' + key] = merged_files_content[key]
         elif key not in file1_content:
             diff_container['+ ' + key] = merged_files_content[key]
-        elif key in file1_content and merged_files_content[key] == file1_content[key]:
+        elif key in file1_content and merged_files_content[key] == file1_content[key]: # noqa E501
             diff_container['  ' + key] = merged_files_content[key]
         else:
             diff_container['- ' + key] = file1_content[key]
@@ -21,7 +21,7 @@ def generate_diff(file_path1, file_path2):
 
     diff_string = '{\n'
     for key, value in diff_container.items():
-        diff_string += '  ' + key.replace("'", "") + ': ' + str(value).lower() + '\n'
+        diff_string += '  ' + key.replace("'", "") + ': ' + str(value).lower() + '\n' # noqa E501
     diff_string += '}'
 
     return diff_string
