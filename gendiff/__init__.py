5343dc486cef662d835121be368cd324dbc112ba
Vladislav <1@zithen.com>
2023-11-03 23:00:09 +0300

    Fix minor issues

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,4 +1,4 @@
-from main_logic.gener_diff import generate_diff
+from gendiff.main_logic.gener_diff import generate_diff
 
 __all__ = (
     generate_diff,
--- gendiff/scripts/gen_diff.py
+++ gendiff/scripts/gen_diff.py
@@ -1,10 +1,10 @@
 from gendiff.main_logic.arg_parse import arg_parser
-from gendiff.main_logic.generate_diff import generate_diff
+from gendiff.main_logic.gener_diff import generate_diff
 
 
 def main():
     args = arg_parser()
-    generate_diff(args.first_file, args.second_file)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
