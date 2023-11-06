8f620f7bdd23db693eb18404376e82fd19d74800
Vladislav <1@zithen.com>
2023-11-07 00:02:47 +0300

    Fix minor issues

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -5,12 +5,19 @@ from gendiff.main_logic.gener_diff import generate_diff
 @pytest.fixture
 def file_path1():
     return 'tests/fixtures/files/file1.json'
+
+
+@pytest.fixture
 def file_path2():
     return 'tests/fixtures/file2.json'
+
+
+@pytest.fixture
 def expected():
     with (open('tests/fixtures/result_generate_diff.txt') as expected):
         return expected
 
+
 def test_basic(file_path1, file_path2):
     result = generate_diff(file_path1, file_path2)
-    assert generate_diff() == expected
+    assert result == expected
