8d47dc7134619185d6e3309cd91734a5594125c8
Vladislav <1@zithen.com>
2023-11-06 22:45:24 +0300

    Change file path in tests

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,8 +1,8 @@
 from gendiff.main_logic.gener_diff import generate_diff
 
 
-FILE_PATH1 = '/home/zithen/education/hexlet_python_projects/python-project-50/gendiff/files/file1.json' # noqa E501
-FILE_PATH2 = '/home/zithen/education/hexlet_python_projects/python-project-50/gendiff/files/file2.json' # noqa E501
+FILE_PATH1 = 'files/file1.json'
+FILE_PATH2 = 'files/file2.json'
 
 def test_basic():
     assert generate_diff(FILE_PATH1, FILE_PATH2) == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
