4f0579f23ca62a754b641bc198a4c551e6957f89
Vladislav <1@zithen.com>
2023-11-07 00:01:32 +0300

    Add fixtures

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -1,9 +1,16 @@
+import pytest
 from gendiff.main_logic.gener_diff import generate_diff
 
 
-FILE_PATH1 = 'gendiff/files/file1.json'
-FILE_PATH2 = 'gendiff/files/file2.json'
+@pytest.fixture
+def file_path1():
+    return 'tests/fixtures/files/file1.json'
+def file_path2():
+    return 'tests/fixtures/file2.json'
+def expected():
+    with (open('tests/fixtures/result_generate_diff.txt') as expected):
+        return expected
 
-
-def test_basic():
-    assert generate_diff(FILE_PATH1, FILE_PATH2) == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}' # noqa E501
+def test_basic(file_path1, file_path2):
+    result = generate_diff(file_path1, file_path2)
+    assert generate_diff() == expected
