c9656160b6ff55f6dd44fd0c3a050ecdc0c14c38
Vladislav <1@zithen.com>
2023-11-06 22:52:20 +0300

    Add blank line

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -4,5 +4,6 @@ from gendiff.main_logic.gener_diff import generate_diff
 FILE_PATH1 = 'gendiff/files/file1.json'
 FILE_PATH2 = 'gendiff/files/file2.json'
 
+
 def test_basic():
     assert generate_diff(FILE_PATH1, FILE_PATH2) == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}' # noqa E501
