21d7f4219aad01d1da979f15459cf14f2c79fa85
Vladislav <1@zithen.com>
2023-11-07 01:18:56 +0300

    Add test coverage badge

--- tests/test_gen_diff.py
+++ tests/test_gen_diff.py
@@ -2,19 +2,24 @@ import pytest
 from gendiff.main_logic.gener_diff import generate_diff
 
 
+FILE_PATH1 = 'tests/fixtures/file1.json'
+FILE_PATH2 = 'tests/fixtures/file2.json'
+RESULT_PATH = 'tests/fixtures/result_generate_diff.txt'
+
+
 @pytest.fixture
 def file_path1():
-    return 'tests/fixtures/file1.json'
+    return FILE_PATH1
 
 
 @pytest.fixture
 def file_path2():
-    return 'tests/fixtures/file2.json'
+    return FILE_PATH2
 
 
 @pytest.fixture
 def expected():
-    with (open('tests/fixtures/result_generate_diff.txt') as expected):
+    with (open(RESULT_PATH) as expected):
         return ('').join(list(expected))
 
 
