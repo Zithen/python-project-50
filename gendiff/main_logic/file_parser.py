76f9ade5923e3b1a96ec331146a67610cea29804
Vladislav <1@zithen.com>
2023-11-10 10:04:55 +0300

    Add raise exception

--- gendiff/main_logic/file_parser.py
+++ gendiff/main_logic/file_parser.py
@@ -13,13 +13,12 @@ def parse_files(file_path1, file_path2):
             file1_content = json.load(file1)
         elif YML in file_path1 or YAML in file_path1:
             file1_content = yaml.load(file1, Loader=yaml.Loader)
-        else:
-            raise RuntimeError('Unsupported file format')
         if JSON in file_path2:
             file2_content = json.load(file2)
         elif YML in file_path2 or YAML in file_path2:
             file2_content = yaml.load(file2, Loader=yaml.Loader)
-        else:
-            raise RuntimeError('Unsupported file format')
-
-    return (file1_content, file2_content)
+    
+    try:
+        return (file1_content, file2_content)
+    except:
+        raise RuntimeError('Unsupported file format')
