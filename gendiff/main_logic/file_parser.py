095c77e0c9908783d8c33fea58442153fe8bb0fa
Vladislav <1@zithen.com>
2023-11-10 18:48:48 +0300

    Refactor file_parser

--- gendiff/main_logic/file_parser.py
+++ gendiff/main_logic/file_parser.py
@@ -2,35 +2,22 @@ import yaml
 import json
 
 
-EXTENSIONS = {
-    'json': 'json.load',
-    'yml': 'yaml.safe_load',
-    'yaml': 'yaml.safe_load',
-} # noqa E123
-JSON, YML, YAML = EXTENSIONS
-JSON_LOAD = '.load'
-YAML_LOAD = '.safe_load'
-
-
-def file_to_dict(file_path, file_extension):
-    pass
+EXTENSIONS_OPERATIONS = {
+    'json': json.load,
+    'yml': yaml.safe_load,
+    'yaml': yaml.safe_load,
+}
 
 
 def parse_files(file_path1, file_path2):
     file1_extension = file_path1.split('.')[-1]
     file2_extension = file_path2.split('.')[-1]
 
-    if file1_extension not in EXTENSIONS.keys() or file2_extension not in EXTENSIONS.keys():
-        raise RuntimeError(f'Unsupported file format, supported formats are {", ".join(EXTENSIONS.keys())}') # noqa E501
+    if file1_extension not in EXTENSIONS_OPERATIONS.keys() or file2_extension not in EXTENSIONS_OPERATIONS.keys(): # noqa E501
+        raise RuntimeError(f'Unsupported file format, supported formats are {", ".join(EXTENSIONS_OPERATIONS.keys())}') # noqa E501
 
     with (open(file_path1) as file1, open(file_path2) as file2):
-        if JSON in file_path1:
-            file1_content = json.load(file1)
-        elif YML in file_path1 or YAML in file_path1:
-            file1_content = yaml.safe_load(file1)
-        if JSON in file_path2:
-            file2_content = json.load(file2)
-        elif YML in file_path2 or YAML in file_path2:
-            file2_content = yaml.safe_load(file2)
+        file1_content = EXTENSIONS_OPERATIONS[file1_extension](file1)
+        file2_content = EXTENSIONS_OPERATIONS[file2_extension](file2)
 
     return (file1_content, file2_content)
