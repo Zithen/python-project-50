0e54a84ae078b7287b1abb728b88a4259a521ed3
Vladislav <1@zithen.com>
2023-11-10 10:09:37 +0300

    Refactor parse_files

--- gendiff/main_logic/file_parser.py
+++ gendiff/main_logic/file_parser.py
@@ -1,4 +1,3 @@
-import re
 import yaml
 import json
 
@@ -7,7 +6,8 @@ JSON = '.json'
 YML = '.yml'
 YAML = '.yaml'
 
-def parse_files(file_path1, file_path2):
+
+def parse_files(file_path1, file_path2): # noqa C901
     with (open(file_path1) as file1, open(file_path2) as file2):
         if JSON in file_path1:
             file1_content = json.load(file1)
@@ -17,8 +17,8 @@ def parse_files(file_path1, file_path2):
             file2_content = json.load(file2)
         elif YML in file_path2 or YAML in file_path2:
             file2_content = yaml.load(file2, Loader=yaml.Loader)
-    
+
     try:
         return (file1_content, file2_content)
-    except:
+    except UnboundLocalError:
         raise RuntimeError('Unsupported file format')
---
+++ gendiff/main_logic/internal_view_creator.py
@@ -0,0 +1,4 @@
+import tests.fixtures.consts
+
+
+def create_inline_view(file1_content, file2_content):
