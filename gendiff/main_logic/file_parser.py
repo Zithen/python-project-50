4438c3d56af6fa70bf86128a7bb2869399974b56
Vladislav <1@zithen.com>
2023-11-10 18:39:51 +0300

    Refactor file_parser

--- gendiff/main_logic/file_parser.py
+++ gendiff/main_logic/file_parser.py
@@ -2,23 +2,35 @@ import yaml
 import json
 
 
-JSON = '.json'
-YML = '.yml'
-YAML = '.yaml'
+EXTENSIONS = {
+    'json': 'json.load',
+    'yml': 'yaml.safe_load',
+    'yaml': 'yaml.safe_load',
+} # noqa E123
+JSON, YML, YAML = EXTENSIONS
+JSON_LOAD = '.load'
+YAML_LOAD = '.safe_load'
 
 
-def parse_files(file_path1, file_path2): # noqa C901
+def file_to_dict(file_path, file_extension):
+    pass
+
+
+def parse_files(file_path1, file_path2):
+    file1_extension = file_path1.split('.')[-1]
+    file2_extension = file_path2.split('.')[-1]
+
+    if file1_extension not in EXTENSIONS.keys() or file2_extension not in EXTENSIONS.keys():
+        raise RuntimeError(f'Unsupported file format, supported formats are {", ".join(EXTENSIONS.keys())}') # noqa E501
+
     with (open(file_path1) as file1, open(file_path2) as file2):
         if JSON in file_path1:
             file1_content = json.load(file1)
         elif YML in file_path1 or YAML in file_path1:
-            file1_content = yaml.load(file1, Loader=yaml.Loader)
+            file1_content = yaml.safe_load(file1)
         if JSON in file_path2:
             file2_content = json.load(file2)
         elif YML in file_path2 or YAML in file_path2:
-            file2_content = yaml.load(file2, Loader=yaml.Loader)
+            file2_content = yaml.safe_load(file2)
 
-    try:
-        return (file1_content, file2_content)
-    except UnboundLocalError:
-        raise RuntimeError('Unsupported file format')
+    return (file1_content, file2_content)
