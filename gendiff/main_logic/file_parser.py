2923b01bdc8516ad4d7aa8ae913a18795c310a54
Vladislav <1@zithen.com>
2023-11-11 17:10:02 +0300

    Add gen diff nested result

--- gendiff/main_logic/file_parser.py
+++ gendiff/main_logic/file_parser.py
@@ -7,14 +7,15 @@ EXTENSIONS_OPERATIONS = {
     'yml': yaml.safe_load,
     'yaml': yaml.safe_load,
 }
+MESSAGE = f'Unsupported file format, supported formats are {", ".join(EXTENSIONS_OPERATIONS)}' # noqa E501
 
 
 def parse_files(file_path1, file_path2):
     file1_extension = file_path1.split('.')[-1]
     file2_extension = file_path2.split('.')[-1]
 
-    if file1_extension not in EXTENSIONS_OPERATIONS.keys() or file2_extension not in EXTENSIONS_OPERATIONS.keys(): # noqa E501
-        raise RuntimeError(f'Unsupported file format, supported formats are {", ".join(EXTENSIONS_OPERATIONS.keys())}') # noqa E501
+    if file1_extension not in EXTENSIONS_OPERATIONS or file2_extension not in EXTENSIONS_OPERATIONS: # noqa E501
+        raise RuntimeError(MESSAGE)
 
     with (open(file_path1) as file1, open(file_path2) as file2):
         file1_content = EXTENSIONS_OPERATIONS[file1_extension](file1)
