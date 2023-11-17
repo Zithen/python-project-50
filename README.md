7c4b05c13b46f76d942343d0317b2c4de181552f
Vladislav <1@zithen.com>
2023-11-17 15:40:01 +0300

    Add my actions to readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [![Actions Status](https://github.com/Zithen/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Zithen/python-project-50/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/4a22630d115785218805/maintainability)](https://codeclimate.com/github/Zithen/python-project-50/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/4a22630d115785218805/test_coverage)](https://codeclimate.com/github/Zithen/python-project-50/test_coverage)
+[![Actions Status](https://github.com/Zithen/python-project-50/actions/workflows/actions.yml/badge.svg)](https://github.com/Zithen/python-project-50/actions/workflows/actions.yml)
--- gendiff/main_logic/internal_view_creator.py
+++ gendiff/main_logic/internal_view_creator.py
@@ -1,20 +1,12 @@
 def create_internal_view(file1_content, file2_content):
-    print('\n')
-    print(file1_content)
-    print('\n')
-    print(file2_content)
-    print('\n')
     added = file2_content.keys() - file1_content.keys()
     removed = file1_content.keys() - file2_content.keys()
-    common = file1_content.keys() & file2_content.keys()
+    unchanged = file1_content.keys() & file2_content.keys()
+    result = []
 
-    for elem in added:
-        depth = 1
-        if not isinstance(elem, dict):
-            return file2_content.get(elem)
-    print(added)
+    print(f'added: {added}')
     print('\n')
     print(removed)
     print('\n')
-    print(common)
+    print(unchanged)
     print('\n')
