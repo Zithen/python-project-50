18801e8fa8a5cf0021c2c048f266b8b831c3204e
Vladislav <1@zithen.com>
2023-11-18 12:03:33 +0300

    Change status bar

--- gendiff/main_logic/internal_view_creator.py
+++ gendiff/main_logic/internal_view_creator.py
@@ -1,8 +1,13 @@
+'''def walk_through_children(key, file_content):
+    if isinstance(key, dict):
+        return
+'''
+
+
 def create_internal_view(file1_content, file2_content):
     added = file2_content.keys() - file1_content.keys()
     removed = file1_content.keys() - file2_content.keys()
     unchanged = file1_content.keys() & file2_content.keys()
-    result = []
 
     print(f'added: {added}')
     print('\n')
