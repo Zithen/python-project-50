ddaaa496831bedffa8937aca8cb776356f68fa35
Vladislav <1@zithen.com>
2023-11-30 01:11:25 +0300

    Fix minor issues

--- gendiff/main_logic/internal_view_creator.py
+++ gendiff/main_logic/internal_view_creator.py
@@ -4,33 +4,34 @@ def create_internal_view(file1_content, file2_content):
     added = file2_content.keys() - file1_content.keys()
     unchanged = file1_content.keys() & file2_content.keys()
 
-    for key in unchanged:
-        file1_value = file1_content[key]
-        file2_value = file2_content[key]
+    for unchanged_key in unchanged:
+        file1_value = file1_content[unchanged_key]
+        file2_value = file2_content[unchanged_key]
         if isinstance(file1_value, dict) and isinstance(file2_value, dict):
-            diff[key] = {
+            diff[unchanged_key] = {
                 'state': 'nested',
-                'value': create_internal_view(file1_value, file2_value), }
+                'value': create_internal_view(file1_value, file2_value),
+            }
         elif file1_value == file2_value:
-            diff[key] = {
+            diff[unchanged_key] = {
                 'state': 'unchanged',
-                'value': file1_content[key],
+                'value': file1_content[unchanged_key],
             }
         else:
-            diff[key] = {
+            diff[unchanged_key] = {
                 'state': 'changed',
                 'old_value': file1_value,
                 'new_value': file2_value,
             }
 
-    for key in removed:
-        diff[key] = {
+    for removed_key in removed:
+        diff[removed_key] = {
             'state': 'removed',
             'value': file1_value,
         }
 
-    for key in added:
-        diff[key] = {
+    for added_key in added:
+        diff[added_key] = {
             'state': 'added',
             'value': file2_value,
         }
